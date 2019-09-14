eedc70d37e3ba5d424f9a8987b7158d3ef0d32b5
sunshineplan <sunshineplan@users.noreply.github.com>
2019-09-14 09:56:34 +0800

    Update assign.py

--- assign.py
+++ assign.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-
 import os
+import sys
 from math import ceil, floor
 from random import choice, shuffle
 
@@ -10,64 +10,77 @@ import click
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_num(num):
-    return eval('{}({})'.format(choice(['ceil', 'floor']), num))
+class Assign:
+    def __init__(self):
+        self._name_list = self.loadNameList()
 
-
-def assign(task, mode):
-    try:
-        with open(os.path.join(here, 'People_List.txt'), encoding='utf-8') as f:
-            people_list = [i.rstrip('\n').strip() for i in f.readlines()]
-            people_list = [i for i in people_list if i != '']
-            people_list = [i for i in people_list if i[0] != '#']
-    except FileNotFoundError:
-        print('People_List.txt is missing. Already create it for you.')
-        with open(os.path.join(here, 'People_List.txt'), 'w', encoding='utf-8') as f:
-            f.write('\n'.join(['Name1', 'Name2', '#Name3', 'Name4']))
-        click.pause()
-        return
-
-    try:
-        per = task//len(people_list)
-    except ZeroDivisionError:
-        print('People_List.txt has no valid content.')
-        click.pause()
-        return
-
-    if task % len(people_list):
-        print('Total:{}, People:{}, Assign:{}-{}'.format(
-            task, len(people_list), per, per+1))
-    else:
-        print('Total:{}, People:{}, Assign:{}'.format(
-            task, len(people_list), per))
-    shuffle(people_list)
-    result = []
-    print_result = []
-    while True:
+    def loadNameList(self):
         try:
-            num = get_num(task/len(people_list))
-            name = people_list.pop(0)
-            result += [name] * num
-            print_result += ['%s: %s' % (name, num)]
-            task -= num
-        except ZeroDivisionError:
-            break
-    if mode == 'random':
-        shuffle(result)
-    with open(os.path.join(here, 'Result.txt'), 'w', encoding='utf-8') as f:
-        f.write('\n'.join(result))
-    print('\n'.join(print_result))
-    click.pause()
+            with open(os.path.join(here, 'Name_List.txt'), encoding='utf-8') as f:
+                name_list = [i.rstrip('\n').strip() for i in f.readlines()]
+                name_list = [i for i in name_list if i != '']
+                name_list = [i for i in name_list if i[0] != '#']
+            if len(name_list) == 0:
+                raise ValueError('Empty list.')
+        except FileNotFoundError:
+            print('Name_List.txt is missing. Already create it for you.')
+            with open(os.path.join(here, 'Name_List.txt'), 'w', encoding='utf-8') as f:
+                f.write('\n'.join(['Name1', 'Name2', '#Name3', 'Name4']))
+            click.pause()
+            sys.exit()
+        except ValueError:
+            click.echo('name_list.txt has no valid content.')
+            click.pause()
+            sys.exit()
+        return name_list
 
 
-@click.command(help='Assign task according people list.')
-@click.argument('task', required=False, default=0, type=click.IntRange(0))
-@click.option('--mode', '-m', default='normal', type=click.Choice(['normal', 'random']), show_default=True, help='Assign task mode.')
-def run(task, mode):
-    if not task:
-        task = click.prompt('Please input total task number',
-                            type=click.IntRange(1))
-    assign(task, mode)
+class AssignByName(Assign):
+    def __init__(self, total, mode):
+        super().__init__()
+        self.total = total
+        self.mode = mode
+        shuffle(self._name_list)
+        if total % len(self._name_list):
+            click.echo('Total:{}, People:{}, Assign:{}-{}'.format(
+                total, len(self._name_list), total//len(self._name_list), total//len(self._name_list)+1))
+        else:
+            click.echo('Total:{}, People:{}, Assign:{}'.format(
+                total, len(self._name_list), total//len(self._name_list)))
+
+    @staticmethod
+    def get_num(num):
+        return eval('{}({})'.format(choice(['ceil', 'floor']), num))
+
+    def run(self):
+        total = self.total
+        result = []
+        print_result = []
+        while True:
+            try:
+                num = self.get_num(total/len(self._name_list))
+                name = self._name_list.pop()
+                result += [name] * num
+                print_result += ['%s: %s' % (name, num)]
+                total -= num
+            except ZeroDivisionError:
+                break
+        if self.mode == 'random':
+            shuffle(result)
+        with open(os.path.join(here, 'Result.txt'), 'w', encoding='utf-8') as f:
+            f.write('\n'.join(result))
+        click.echo('\n'.join(print_result))
+        click.pause()
+
+
+@click.command(help='Assign by name.')
+@click.argument('total', required=False, default=0, type=click.IntRange(0))
+@click.option('--mode', '-m', default='normal', type=click.Choice(['normal', 'random']), show_default=True, help='Assign mode.')
+def run(total, mode):
+    if not total:
+        total = click.prompt('Please input total number',
+                             type=click.IntRange(1))
+    AssignByName(total, mode).run()
 
 
 if __name__ == '__main__':
